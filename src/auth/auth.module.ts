31fe87aef7b7ba012a6c63cd528c1e0977e586eb
joonas-Birrer <Jonas.birrer08@gmail.com>
2026-01-14 12:48:55 +0100

    formatted

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthController } from './controller/auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { AuthGuard } from './auth.guard';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
